ea488f031349dba826234dccd7b5f3c1715a4900
zsnyder21 <64924773+zsnyder21@users.noreply.github.com>
2021-08-02 15:59:45 -0600

    Added some methods for finding a single route by URL and finding climbs by specified lat/long

--- src/RouteDataPipeline.py
+++ src/RouteDataPipeline.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import re
 
 from geopy import geocoders
 from dotenv import load_dotenv
@@ -22,7 +23,7 @@ class RoutePipeline(object):
         self.cursor.close()
         self.connection.close()
 
-    def getRoutesByCityState(self, city: str, state: str, maximumDistance: float, distanceUnits: str = "mi"):
+    def fetchRoutesByCityState(self, city: str, state: str, maximumDistance: float, distanceUnits: str = "mi"):
         if distanceUnits.lower() not in {"mi", "miles", "km", "kilometers"}:
             raise ValueError("distanceUnits must be on of the following: ['mi', 'miles', 'km', 'kilometers'].")
 
@@ -34,8 +35,6 @@ class RoutePipeline(object):
         cityStateLocation = self.geoAgent.geocode(query=", ".join([city.strip(), state.strip()]), exactly_one=True)
         latitude = cityStateLocation.raw["lat"]
         longitude = cityStateLocation.raw["lon"]
-
-        print(cityStateLocation)
 
         query = f"""
         select r.*
@@ -49,11 +48,61 @@ class RoutePipeline(object):
                 on true
             where a.Latitude is not null
                 and a.Longitude is not null
+                and a.AreaId != 112166257  -- Filter out generic area
                 and d.Distance <= {maximumDistance}
         """
 
         self.cursor.execute(query)
         return self.cursor.fetchall()
+
+    def fetchRoutesByLatLong(self, latitude: float, longitude: str, maximumDistance: float, distanceUnits: str = "mi"):
+        if distanceUnits.lower() not in {"mi", "miles", "km", "kilometers"}:
+            raise ValueError("distanceUnits must be on of the following: ['mi', 'miles', 'km', 'kilometers'].")
+
+        if distanceUnits.lower() in {"km", "kilometers"}:
+            earthRadius = 6371.0
+        else:
+            earthRadius = 3958.8
+
+        query = f"""
+        select r.*
+            from Routes r
+            inner join Areas a
+                on a.AreaId = r.AreaId
+            left join lateral (
+                select {earthRadius} * 2 * asin(sqrt(sin((radians(a.Latitude) - radians({latitude})) / 2) ^ 2 
+            + cos(radians({latitude})) * cos(radians(a.Latitude)) * sin((radians(a.Longitude) - radians({longitude})) / 2) ^ 2)) as Distance
+            ) d
+                on true
+            where a.Latitude is not null
+                and a.Longitude is not null
+                and a.AreaId != 112166257  -- Filter out generic area
+                and d.Distance <= {maximumDistance}
+        """
+
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def fetchRouteByURL(self, routeURL: str):
+        routeId = re.search(pattern=r"\d+", string=routeURL)
+
+        if routeId is not None:
+            routeId = int(routeId.group(0))
+        else:
+            raise ValueError(f"Error: Cannot parse RouteId from specified URL ({routeURL}).")
+
+        query = f"""
+        select  a.Latitude,
+                a.Longitude,
+                r.*
+            from Routes r
+            inner join Areas a
+                on a.AreaId = r.AreaId
+            where r.RouteId = {routeId}
+        """
+
+        self.cursor.execute(query)
+        return self.cursor.fetchone()
 
 
 if __name__ == "__main__":
@@ -68,11 +117,15 @@ if __name__ == "__main__":
         geopyUsername="zsnyder21"
     )
 
-    routes = pipe.getRoutesByCityState(
-        city="Tempe",
-        state="AZ",
-        maximumDistance=25,
-        distanceUnits="mi"
-    )
+    # routes = pipe.getRoutesByCityState(
+    #     city="Boulder",
+    #     state="CO",
+    #     maximumDistance=25,
+    #     distanceUnits="mi"
+    # )
 
-    print(len(routes))
+    route = pipe.fetchRouteByURL(routeURL="https://www.mountainproject.com/route/105750457/cosmosis")
+    print(route[0], route[1])
+
+    test = pipe.fetchRoutesByLatLong(latitude=route[0], longitude=route[1], maximumDistance=50, distanceUnits="miles")
+    print(test)
